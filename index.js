120e165bb1f6a0f50c7a7b32000b682beffdf097
Samuel San Nicolas <samuelsan3@gmail.com>
2017-10-22 00:09:43 -0700

    Mobile nav bar

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+$(document).ready(() => {
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 300
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true, // Choose whether you can drag to open on touch screens
+      }
+    );
+});
+
 // Get image + create tags
 
 // Sentient analysis
