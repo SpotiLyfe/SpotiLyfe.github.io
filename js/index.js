2a0c362399257a1a0e65970adc3a349521f54047
Nanda Sundaresan <nsundaresan@D-173-250-146-190.dhcp4.washington.edu>
2017-10-22 08:52:40 -0700

    adding widget

--- js/index.js
+++ js/index.js
@@ -57,34 +57,39 @@ getLightness(url, function(b, g) {
 // Genre classification
 
 // country songs
-var link1 = "https://open.spotify.com/track/7GWo0tyrAT1e3TwpxWqFE1"
-var link2 = "https://open.spotify.com/track/54EWDYWhs4w6SODnxabuoh"
-var link3 = "https://open.spotify.com/track/7zOoHxDt1duHL8czY7o7ab"
+var link1 = "https://open.spotify.com/track/7GWo0tyrAT1e3TwpxWqFE1";
+var link2 = "https://open.spotify.com/track/54EWDYWhs4w6SODnxabuoh";
+var link3 = "https://open.spotify.com/track/7zOoHxDt1duHL8czY7o7ab";
 
 // "urban" songs
-var link4 = "https://open.spotify.com/track/2y0zglZs6AlNBg4XEm2leW"
-var link5 = "https://open.spotify.com/track/5ItzU5pBrFmRUudfr5RkJP"
-var link6 = "https://open.spotify.com/track/5uImkHXfTLkNYwemtGH7kB"
+var link4 = "https://open.spotify.com/track/2y0zglZs6AlNBg4XEm2leW";
+var link5 = "https://open.spotify.com/track/5ItzU5pBrFmRUudfr5RkJP";
+var link6 = "https://open.spotify.com/track/5uImkHXfTLkNYwemtGH7kB";
 
-var songs = [link1, link2, link3, link4, link5, link6]
+var songs = [link1, link2, link3, link4, link5, link6];
 
+
+var oauth = "BQADjP6FZY3rpenUjwF7i7zjANng-hoDGtIeAzH_xJZZUlDO-Tajap_z0XlAbkz0kD5FJZwiPirTP01Y_6HFipRIPYmKQnQ1DQYzlJ923BMqIEDFLxQoxLiXiPN6JVi3D9-drDo568pdfYNDPgTpgA8zl4mQf89ATrUlngD07ukVVfhZHC9r8FiGXrK1GYZ2YOxefvLkkEC1";
+
+function addVideoElement(url) {
+  document.getElementById("widget").innerHTML =
+  '<iframe src="https://open.spotify.com/embed?uri=spotify:user:erebore:playlist:788MOXyTfcUb1tdw4oC7KJ" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>';
+}
 
 function randomNum() {
-  return Math.floor(Math.random() * 3)
+  return Math.floor(Math.random() * 3);
 }
 
 // Generate random song from classification:
 function getSong(response) {
-  var responseData = JSON.parse(response)
-  console.log(responseData["tracks"]["items"]["external_urls"][randomNum()])
+  var responseData = JSON.parse(response);
+  console.log(responseData["tracks"]["items"]["external_urls"][randomNum()]);
 }
 
 function spotify(genre){
   if(genre == "country") {
-    return songs[randomNum()]
-    console.log(songs[randomNum()])
+    addVideoElement(songs[randomNum()])
   } else {
-    return songs[randomNum() + 3]
-    console.log(songs[randomNum() + 3])
+    addVideoElement(songs[randomNum() + 3]);
   }
 }
