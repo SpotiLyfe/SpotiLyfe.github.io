9d683d4fdedc9a7ac36d8a07386c341110315c69
Nanda Sundaresan <nsundaresan@D-173-250-146-190.dhcp4.washington.edu>
2017-10-22 00:02:54 -0700

    added songs

--- index.js
+++ index.js
@@ -4,4 +4,17 @@
 
 // Genre classification
 
+// country songs
+var link1 = "https://open.spotify.com/track/7GWo0tyrAT1e3TwpxWqFE1"
+var link2 = "https://open.spotify.com/track/54EWDYWhs4w6SODnxabuoh"
+var link3 = "https://open.spotify.com/track/7zOoHxDt1duHL8czY7o7ab"
+
+// "urban" songs
+var link4 = "https://open.spotify.com/track/2y0zglZs6AlNBg4XEm2leW"
+var link5 = "https://open.spotify.com/track/5ItzU5pBrFmRUudfr5RkJP"
+var link6 = "https://open.spotify.com/track/5uImkHXfTLkNYwemtGH7kB"
+
+var country_songs = [link1, link2, link3]
+var urban_songs = [link4, link5, link6]
+
 // Spotify song + play song
