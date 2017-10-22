da38c66d032a591f6d58f1608a7ed31ab2add89a
Soham <sohampardeshi@gmail.com>
2017-10-22 02:52:53 -0700

    Geolocation

--- js/auth.js
+++ js/auth.js
@@ -1,2 +1,22 @@
 const SPOTIFY_CLIENT_ID = '964b4736722e41de899fb3faac5c904c';
 const SPOTIFY_CLIENT_SECRET = 'f68e2046d7454d128c48d840acdff617';
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+    var crd = pos.coords;
+
+    alertify.warning(`Latitude : ${crd.latitude}`);
+    alertify.warning(`Longitude: ${crd.longitude}`);
+    alertify.warning(`More or less ${crd.accuracy} meters.`);
+};
+
+function error(err) {
+    alertify.error(`ERROR(${err.code}): ${err.message}`);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
