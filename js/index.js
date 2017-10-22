9ebec3c93d412b4051ef03f2091b9d274c62a235
Nanda Sundaresan <nsundaresan@D-173-250-146-190.dhcp4.washington.edu>
2017-10-22 14:14:18 -0700

    added more songs

--- js/index.js
+++ js/index.js
@@ -38,41 +38,31 @@ function getLightness(imageSrc, callback) {
     }
 }
 
-
-// Sentiment analysis
-
-// Genre classification
-
 // country songs
-
 var link1 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A7GWo0tyrAT1e3TwpxWqFE1";
 var link2 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A54EWDYWhs4w6SODnxabuoh";
 var link3 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A7zOoHxDt1duHL8czY7o7ab";
+var link4 = 'https://open.spotify.com/embed?uri=spotify%3Atrack%3A5jROdl6MhcmP3O7h2sVgtw'
+var link5 = 'https://open.spotify.com/embed?uri=spotify%3Atrack%3A0ipcmHor30Y8p6hCd8QUVq'
+var link6 = 'https://open.spotify.com/embed?uri=spotify%3Atrack%3A5j4hSQH0KaAc8f6cimnXIT'
 
 // "urban" songs
-var link4 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A2y0zglZs6AlNBg4XEm2leW";
-var link5 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A5ItzU5pBrFmRUudfr5RkJP";
-var link6 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A5uImkHXfTLkNYwemtGH7kB";
+var link7 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A2y0zglZs6AlNBg4XEm2leW";
+var link8 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A5ItzU5pBrFmRUudfr5RkJP";
+var link9 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A5uImkHXfTLkNYwemtGH7kB";
+var link10 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A76IkF7pA7RscjDTvOFT4vb"
+var link11 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A3gWu8y0TKCIdy2mpTqVnTl";
+var link12 = "https://open.spotify.com/embed?uri=spotify%3Atrack%3A2QAYzr2tstl81RiI7a5nfI";
 
-var songs = [link1, link2, link3, link4, link5, link6];
-
-console.log(imageMetaData);
-
-var oauth = "BQADjP6FZY3rpenUjwF7i7zjANng-hoDGtIeAzH_xJZZUlDO-Tajap_z0XlAbkz0kD5FJZwiPirTP01Y_6HFipRIPYmKQnQ1DQYzlJ923BMqIEDFLxQoxLiXiPN6JVi3D9-drDo568pdfYNDPgTpgA8zl4mQf89ATrUlngD07ukVVfhZHC9r8FiGXrK1GYZ2YOxefvLkkEC1";
+var songs = [link1, link2, link3, link4, link5, link6, link7, link8, link9, link10, link11, link12];
 
 function addVideoElement(url) {
   document.getElementById("widget").innerHTML =
-  '<iframe src="' + url + '" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>';
+  '<iframe src="' + url + '" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>';
 }
 
 function randomNum() {
   return Math.floor(Math.random() * 3);
-}
-
-// Generate random song from classification:
-function getSong(response) {
-  var responseData = JSON.parse(response);
-  console.log(responseData["tracks"]["items"]["external_urls"][randomNum()]);
 }
 
 function spotify(genre){
