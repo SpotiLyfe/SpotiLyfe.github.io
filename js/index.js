5c2c0801219a7285a247070174ef6b1e84146611
Nanda Sundaresan <nsundaresan@D-173-250-146-190.dhcp4.washington.edu>
2017-10-22 04:42:53 -0700

    update

--- js/index.js
+++ js/index.js
@@ -68,50 +68,104 @@ var link6 = "https://open.spotify.com/track/5uImkHXfTLkNYwemtGH7kB"
 
 var songs = [link1, link2, link3, link4, link5, link6]
 
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
+function getUsername(callback) {
+  console.log('getUsername');
+	var url = 'https://api.spotify.com/v1/me';
+	$.ajax(url, {
+		dataType: 'json',
+		headers: {
+			'Authorization': 'Bearer ' + g_access_token
+		},
+		success: function(r) {
+			console.log('got username response', r);
+			callback(r.id);
+		},
+		error: function(r) {
+			callback(null);
+		}
+	});
+}
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
+function createPlaylist(username, name, callback) {
+	console.log('createPlaylist', username, name);
+	var url = 'https://api.spotify.com/v1/users/' + username +
+		'/playlists';
+	$.ajax(url, {
+		method: 'POST',
+		data: JSON.stringify({
+			'name': name,
+			'public': false
+		}),
+		dataType: 'json',
+		headers: {
+			'Authorization': 'Bearer ' + g_access_token,
+			'Content-Type': 'application/json'
+		},
+		success: function(r) {
+			console.log('create playlist response', r);
+			callback(r.id);
+		},
+		error: function(r) {
+			callback(null);
+		}
+	});
+}
 
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/users/nsundaresan',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body);
-    });
-  }
-});
-
-var spotifyApi = new SpotifyWebApi({
-  clientId : SPOTIFY_CLIENT_ID,
-  clientSecret : SPOTIFY_CLIENT_SECRET,
-  redirectUri : 'http://spotilyfe.github.io'
-});
-
-spotifyApi.setAccessToken(oauthToken)
-module.exports = function (n) { return n * 111 }
-
-spotifyApi.searchTracks('Country')
-  .then(function(data) {
-    console.log('Search by "Country"', data.body);
-  }, function(err) {
-    console.error(err);
-  });
+function addTracksToPlaylist(username, playlist, tracks, callback) {
+	console.log('addTracksToPlaylist', username, playlist, tracks);
+	var url = 'https://api.spotify.com/v1/users/' + username +
+		'/playlists/' + playlist +
+		'/tracks'; // ?uris='+encodeURIComponent(tracks.join(','));
+	$.ajax(url, {
+		method: 'POST',
+		data: JSON.stringify(tracks),
+		dataType: 'text',
+		headers: {
+			'Authorization': 'Bearer ' + g_access_token,
+			'Content-Type': 'application/json'
+		},
+		success: function(r) {
+			console.log('add track response', r);
+			callback(r.id);
+		},
+		error: function(r) {
+			callback(null);
+		}
+	});
+}
+function doit() {
+	// parse hash
+	var hash = location.hash.replace(/#/g, '');
+	var all = hash.split('&');
+	var args = {};
+	console.log('all', all);
+	all.forEach(function(keyvalue) {
+		var idx = keyvalue.indexOf('=');
+		var key = keyvalue.substring(0, idx);
+		var val = keyvalue.substring(idx + 1);
+		args[key] = val;
+	});
+	g_name = localStorage.getItem('createplaylist-name');
+	g_tracks = JSON.parse(localStorage.getItem('createplaylist-tracks'));
+	console.log('got args', args);
+	if (typeof(args['access_token']) != 'undefined') {
+		// got access token
+		console.log('got access token', args['access_token']);
+		g_access_token = args['access_token'];
+	}
+	getUsername(function(username) {
+		console.log('got username', username);
+		createPlaylist(username, g_name, function(playlist) {
+			console.log('created playlist', playlist);
+			addTracksToPlaylist(username, playlist, g_tracks, function() {
+				console.log('tracks added.');
+				$('#playlistlink').attr('href', 'spotify:user:'+username+':playlist:'+playlist);
+				$('#creating').hide();
+				$('#done').show();
+			});
+		});
+	});
+}
 
 // random num used for random song from json returned from get
 function randomNum() {
